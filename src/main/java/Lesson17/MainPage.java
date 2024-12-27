c59fc1a105d97404a04ff34804ad27f10f24efe5
TalkativeCat <dddd@ddd.com>
2024-12-27 14:58:03 +0500

    Распил страницы тестов по Page Object

---
+++ src/main/java/Lesson17/MainPage.java
@@ -0,0 +1,40 @@
+package Lesson17;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MainPage {
+    private WebDriver driver;
+
+    private final String mainUrl = "https://www.mts.by/";
+    private final By acceptCookieButton = By.xpath("//h3[contains(text(), 'Обработка файлов cookie')]/../../..//button[text() = 'Принять']");
+    public final WebElement paymentBlockTitle = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2"));
+
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        driver.manage().window().maximize();
+
+    }
+    public WebElement paymentLogo(String cardName) {
+        return driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение')]/..//img[@alt='" + cardName + "']"));
+    }
+
+
+
+    public void goToMainPage() {
+        driver.get(mainUrl);
+    }
+    public void acceptCookies() {
+        if(driver.findElement(acceptCookieButton).isDisplayed()) {
+            driver.findElement(acceptCookieButton).click();
+        }
+    }
+    public WebDriver getCurrentDriver() {
+        return driver;
+    }
+
+
+
+}
---
+++ src/test/java/Lesson15/NewMtsTest.java
@@ -0,0 +1,54 @@
+package Lesson15;
+
+import Lesson17.MainPage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NewMtsTest {
+    static MainPage mainPage;
+    /**
+     * Выполняем данный метод первым до начала всех тестов
+     */
+    @BeforeAll
+    public static void setup() {
+        mainPage = new MainPage(new ChromeDriver());
+        mainPage.goToMainPage();
+        mainPage.acceptCookies();
+    }
+    /**
+     * Выполняем данный метод последним, не зависимо от результатов тестов
+     */
+    @AfterAll
+    public static void teardown() {
+        if (mainPage.getCurrentDriver() != null) {
+            mainPage.getCurrentDriver().quit();
+        }
+    }
+    /**
+     * Ищем блок "Онлайн пополнение без комиссии" и проверяем его заголовок
+     */
+    @DisplayName("Проверка названия блока")
+    @ParameterizedTest
+    @ValueSource(strings = {"Онлайн пополнение\nбез комиссии"})
+    public void checkBlockHeader(String expectedTitle) {
+        assertEquals(expectedTitle, mainPage.paymentBlockTitle.getText());
+    }
+    /**
+     * Проверяем, отображаются ли логотипы платежных систем
+     */
+    @DisplayName("Проверка отображения логотипов платежных систем")
+    @ParameterizedTest
+    @ValueSource(strings = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"})
+    public void checkPaymentLogos(String cardName) {
+        assertTrue(mainPage.paymentLogo(cardName).isDisplayed());
+    }
+
+
+}
